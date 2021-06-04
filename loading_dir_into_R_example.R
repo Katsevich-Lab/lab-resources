33aa889145c7e64c12ab2dc972e5ea5dbe353d49
Tim <tbarry2@andrew.cmu.edu>
2021-06-03 23:12:53 -0400

    fix xie dirname

--- loading_dir_into_R_example.R
+++ loading_dir_into_R_example.R
@@ -1,5 +1,5 @@
 # an example of loading a file path stored within the config file into R
 source("~/.get_config_path.R")
 local_glmeiv <- .get_config_path("LOCAL_GLMEIV_DATA_DIR")
-local_xie <- .get_config_path("LOCAL_XIE_2019_DATA_DIR")
+local_xie_2019 <- .get_config_path("LOCAL_XIE_2019_DATA_DIR")
 remote_gasperini_2019 <- .get_config_path("REMOTE_GASPERINI_2019_DATA_DIR")
