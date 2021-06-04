651b94aa07d29866a4d40f7ca92186e123548ca9
Tim <tbarry2@andrew.cmu.edu>
2021-06-04 10:55:38 -0400

    Stop sourcing .get_config_path in example; it loads automatically from .Rprofile.

--- loading_dir_into_R_example.R
+++ loading_dir_into_R_example.R
@@ -1,5 +1,8 @@
-# an example of loading a file path stored within the config file into R
-source("~/.get_config_path.R")
+# An example of loading file paths stored within the .reseaerch_config file into R.
+
 local_glmeiv <- .get_config_path("LOCAL_GLMEIV_DATA_DIR")
 local_xie_2019 <- .get_config_path("LOCAL_XIE_2019_DATA_DIR")
 remote_gasperini_2019 <- .get_config_path("REMOTE_GASPERINI_2019_DATA_DIR")
+
+# In Rstudio, click the broom icon to clear all variables from the global environment.
+# Make sure "Include hidden objects" is NOT checked to retain the .get_config_path function.
